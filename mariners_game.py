07e6cd053cc46c78a8a8217e22adeb0787c46270
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-06 21:45:20 -0700

    Update mariners_game.py

--- mariners_game.py
+++ mariners_game.py
@@ -1,11 +1,11 @@
 #   Eli Gatchalian
 #   May 6, 2019
 
-import sys
 import statsapi
 from post_mariners_tweet import post_tweet
 from datetime import datetime
 
+#   Get all of the team's games today and determine if a tweet is ready to be sent out or not
 def todays_games():
     todays_date = datetime.today().strftime('%m/%d/%Y')
     game_day = statsapi.schedule(team=136,start_date=todays_date)
@@ -13,13 +13,15 @@ def todays_games():
     if(bool(game_day)):
         for game in range(len(game_day)): #Handling Doubleheader days
             single_game = game_day[game]
-            tweet_game(single_game['game_id'], single_game)
+            tweet_game(single_game['game_id'], single_game['status'])
     else:
         print('There is no ' + statsapi.get('team', {'teamId':136})['teams'][0]['name'] + ' game today.')       
 
-def tweet_game(game_id, game_day):
-    print(statsapi.linescore(game_id))
-    if(game_day['status'] == 'Final' or game_day['status'] == 'Game Over'):
+#   Post a tweet if the game has concluded
+def tweet_game(game_id, game_status):
+    print(statsapi.linescore(game_id)) #Display the game's linebox
+    
+    if(game_status == 'Final' or game_status == 'Game Over'):
         team_info = statsapi.get('game', {'gamePk':game_id, 'teamId': 136})['gameData']['teams']
         
         if(team_info['away']['id'] == 136):
