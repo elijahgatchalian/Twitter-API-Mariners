829360a755c396f07ad7a6dfdf5d1739ee0f6591
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-06 21:22:06 -0700

    Update mariners_game.py
    Reformatted to handle doubleheaders

--- mariners_game.py
+++ mariners_game.py
@@ -6,26 +6,28 @@ import statsapi
 from post_mariners_tweet import post_tweet
 from datetime import datetime
 
-todays_date = datetime.today().strftime('%m/%d/%Y')
-game_day = statsapi.schedule(team=136,start_date=todays_date)
-
-if(bool(game_day)):
-    game_day = game_day[0]
-    game_day_id = game_day['game_id']
-else:
-    print('There is no ' + statsapi.get('team', {'teamId':136})['teams'][0]['name'] + ' game today.')
-    sys.exit()
-           
-print(statsapi.linescore(game_day_id)) #Display game's linescore
-
-if(game_day['status'] == 'Final'):
-    team_info = statsapi.get('game', {'gamePk':game_day_id, 'teamId': 136})['gameData']['teams']
+def todays_games():
+    todays_date = datetime.today().strftime('%m/%d/%Y')
+    game_day = statsapi.schedule(team=136,start_date=todays_date)
     
-    if(team_info['away']['id'] == 136):
-        record = team_info['away']['record']['leagueRecord']
+    if(bool(game_day)):
+        for game in range(len(game_day)): #Handling Doubleheader days
+            single_game = game_day[game]
+            tweet_game(single_game['game_id'], single_game)
     else:
-        record = team_info['home']['record']['leagueRecord']
-    
-    results = post_tweet(record['wins'],record['losses'])
-    print(results)
-    
+        print('There is no ' + statsapi.get('team', {'teamId':136})['teams'][0]['name'] + ' game today.')       
+
+def tweet_game(game_id, game_day):
+    print(statsapi.linescore(game_id))
+    if(game_day['status'] == 'Final' or game_day['status'] == 'Game Over'):
+        team_info = statsapi.get('game', {'gamePk':game_id, 'teamId': 136})['gameData']['teams']
+        
+        if(team_info['away']['id'] == 136):
+            record = team_info['away']['record']['leagueRecord']
+        else:
+            record = team_info['home']['record']['leagueRecord']
+            
+        results = post_tweet(record['wins'],record['losses'])
+        print(results)
+        
+todays_games()
