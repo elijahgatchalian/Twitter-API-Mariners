4f7f6da77e50a6139801daf3d4136d52849da7b0
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-10 08:46:22 -0700

    Update mariners_game.py
    Moving most of the functionality to new file: game_status_methods.py

--- mariners_game.py
+++ mariners_game.py
@@ -2,55 +2,26 @@
 #   May 6, 2019
 
 import statsapi
-from post_mariners_tweet import post_tweet
+import game_status_methods as gsm
 from datetime import datetime
 
-#   Return the team's wins and losses resulting from the game
-def team_record(game_info):
-    team_info = game_info['gameData']['teams']
-    
-    if(team_info['away']['id'] == 136):
-        record = team_info['away']['record']['leagueRecord']
-    else:
-        record = team_info['home']['record']['leagueRecord']
-        
-    return record['wins'], record['losses']
+#   Display all of the team's games today
+todays_date = datetime.today().strftime('%m/%d/%Y')
+game_day = statsapi.schedule(team=136,start_date=todays_date)
 
-#   Get all of the team's games today and determine if a tweet is ready to be sent out or not
-def todays_games():
-    todays_date = datetime.today().strftime('%m/%d/%Y')
-    game_day = statsapi.schedule(team=136,start_date=todays_date)
+if(bool(game_day)):
+    print()
+    for game in range(len(game_day)): #Handling Doubleheader days
+        game_info = statsapi.get('game', {'gamePk':game_day[game]['game_id'], 'teamId':136})
+        run_method = gsm.status_to_method.get(game_day[game]['status'])
+        if(str(run_method) == 'None'):
+            run_method = gsm.status_to_method.get('Unknown')
+            run_method(game_day[game]['status'])
+        else:
+            run_method(game_info)
+        
+        print('\n' + statsapi.linescore(game_info['gamePk']) + '\n') #Display the game's linebox
+else:
+    team_name = statsapi.get('team', {'teamId':136})['teams'][0]['name']
+    print('There is no ' + team_name + ' game today.')
     
-    if(bool(game_day)):
-        for game in range(len(game_day)): #Handling Doubleheader days
-            print()
-            game_info = statsapi.get('game', {'gamePk':game_day[game]['game_id'], 'teamId':136})
-            ballgame(game_info, game_day[game]['status'])
-    else:
-        team_name = statsapi.get('team', {'teamId': 136})['teams'][0]['name']
-        print('There is no ' + team_name + ' game today.')        
-
-#   Show status of the game
-def ballgame(game_info, game_status):
-    if(game_status in ['Final', 'Game Over']):
-        wins, losses = team_record(game_info)
-        print(post_tweet(wins, losses))
-    elif(game_status != 'Postponed'):
-        live_game = game_info['liveData']['linescore']
-        inning = live_game['currentInningOrdinal']
-        inning_state = live_game['inningState']
-        print(inning_state + ' of the ' + inning)
-        if(inning_state not in ['Middle', 'End']):
-            batter = live_game['offense']['batter']['fullName']
-            # Count
-            balls = str(live_game['balls'])
-            strikes = str(live_game['strikes'])
-            outs = str(live_game['outs'])
-            print('At bat: ' + batter)
-            print(balls + '-' + strikes + ', ' + outs + ' out(s)')
-    else:
-        print('This game is postponed.')
-        
-    print('\n' + statsapi.linescore(game_info['gamePk']) + '\n') #Display the game's linebox
-        
-todays_games()
