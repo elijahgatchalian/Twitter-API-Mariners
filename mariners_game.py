602d8accd952d659b4ab6adde6df03aa641cb622
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-16 13:34:58 -0700

    Update mariners_game.py
    Changed to instead look at size of game_day.

--- mariners_game.py
+++ mariners_game.py
@@ -3,15 +3,20 @@
 
 import statsapi
 from game_status_methods import status_to_method
-from datetime import datetime
+from datetime import datetime  
 
 #   Display all of the team's games today
 todays_date = datetime.today().strftime('%m/%d/%Y')
 game_day = statsapi.schedule(team=136,start_date=todays_date)
 
-if(bool(game_day)):
-    print()
-    for game in range(len(game_day)): #Handling Doubleheader days
+print()
+
+num_games = len(game_day)
+if(num_games == 0):
+    team_name = statsapi.get('team', {'teamId':136})['teams'][0]['name']
+    print('There is no ' + team_name + ' game today.\n')
+else:
+    for game in range(num_games): #Handling Doubleheader days
         game_info = statsapi.get('game', {'gamePk':game_day[game]['game_id'], 'teamId':136})
         run_method = status_to_method.get(game_day[game]['status'])
         if(str(run_method) == 'None'):
@@ -21,7 +26,3 @@ if(bool(game_day)):
             run_method(game_info)
         
         print('\n' + statsapi.linescore(game_info['gamePk']) + '\n') #Display the game's linebox
-else:
-    team_name = statsapi.get('team', {'teamId':136})['teams'][0]['name']
-    print('There is no ' + team_name + ' game today.')
-    
