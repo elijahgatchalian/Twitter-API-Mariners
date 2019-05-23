40c3b38ca66436273ccd1041a0c2ec0b519f7a1a
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-05-23 13:17:13 -0700

    Update mariners_game.py
    Using status_to_method when there is no game that day.

--- mariners_game.py
+++ mariners_game.py
@@ -1,7 +1,7 @@
 #   Eli Gatchalian
 #   May 6, 2019
 
-import statsapi
+from statsapi import get, linescore, schedule
 from game_status_methods import status_to_method
 from datetime import datetime  
 
@@ -12,16 +12,16 @@ game_day = statsapi.schedule(team=136,start_date=todays_date)
 print()
 
 num_games = len(game_day)
+
 if(num_games == 0):
-    team_name = statsapi.get('team', {'teamId':136})['teams'][0]['name']
-    print('There is no ' + team_name + ' game today.\n')
+    status_to_method.get('No Game Today')()
 else:
     for game in range(num_games): #Handling Doubleheader days
         game_info = statsapi.get('game', {'gamePk':game_day[game]['game_id'], 'teamId':136})
         run_method = status_to_method.get(game_day[game]['status'])
+        
         if(str(run_method) == 'None'):
-            run_method = status_to_method.get('Unknown')
-            run_method(game_day[game]['status'])
+            status_to_method.get('Unknown')(game_day[game]['status'])
         else:
             run_method(game_info)
         
