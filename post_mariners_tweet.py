e63099593eafe6624f294ad9c8bab4cb124ebe3f
Eli Gatchalian <44270523+elijahgatchalian@users.noreply.github.com>
2019-06-26 15:30:47 -0700

    Update post_mariners_tweet.py
    Small changes. Biggest change was how previous record is looked at in check_previous_tweet. I found if I tweeted on my phone rather than through this program, the tweet would post as 'W-L' rather than '@screen_name W-L'.

--- post_mariners_tweet.py
+++ post_mariners_tweet.py
@@ -1,24 +1,24 @@
 #   Eli Gatchalian
 #   May 4, 2019
 
-import tweepy
-
 FILENAME = "seattleMariners.txt"
 
 #   Authenticate keys from developer.twitter.com
 def authenticate_keys():
     from twitterKeys import CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_KEY, ACCESS_KEY_SECRET
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
+    from tweepy import OAuthHandler, API, parsers
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_KEY_SECRET)
-    return tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    return API(auth, parser=parsers.JSONParser())
 
 #   Return True if previous tweet does not match the W-L record we are trying to tweet now.
 #   Return False otherwise.
-def check_previous_tweet(wins, losses, tweet_id, api):
+def check_previous_tweet(wins, losses, tweet_id, api, screen_name):
     prev_tweet = api.get_status(tweet_id)['text']
     prev_tweet = prev_tweet.replace('-', ' ').split(' ')
-    return prev_tweet[1] != str(wins) or prev_tweet[2] != str(losses)
-
+    size = len(prev_tweet) - 1
+    return prev_tweet[size - 1] != str(wins) or prev_tweet[size] != str(losses)
+            
 #   Write the new tweet_id to FILENAME
 def write_to_file(tweet_id):
     f = open(FILENAME, 'w')
@@ -26,21 +26,20 @@ def write_to_file(tweet_id):
     f.close()
 
 #   Post tweet of the team's record after today's game results.
-#   Return the results of posting the tweet.
+#   Print the results of posting the tweet.
 def post_tweet(wins, losses):
     tweet_id = open(FILENAME).read()
     api = authenticate_keys()
     screen_name = api.me()['screen_name']
     url = 'https://twitter.com/' + screen_name + '/status/'
+    results = 'This game was already tweeted.\n' + url + tweet_id
     
-    if(check_previous_tweet(wins,losses,tweet_id,api)):
+    if(check_previous_tweet(wins,losses,tweet_id,api,screen_name)):
         from webbrowser import open_new_tab
         new_tweet = api.update_status('@' + screen_name + ' ' + str(wins) + '-' + str(losses), tweet_id)
         tweet_id = new_tweet['id_str'] #Set tweet_id to newest tweet's id
         write_to_file(tweet_id)
         results = 'Tweet successfully posted.'
         open_new_tab(url + tweet_id)
-    else:
-        results = 'This game was already tweeted.\n' + url + tweet_id
         
-    print(results)
+    print(results + '\n')
